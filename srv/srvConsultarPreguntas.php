59ad003649fa3a9f502e4d12bddf07c0d881213b
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-03-05 09:51:22 -0600

    Mostrar datos

--- srv/srvConsultarPreguntas.php
+++ srv/srvConsultarPreguntas.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../lib/php/ejecuta.php";
-require_once __DIR__ . "/../lib/php/consultarPregunta.php";
+require_once __DIR__ . "/dao/consultarPregunta.php";
 
 ejecuta(function (){
   $lista = consultarPregunta();
@@ -14,7 +14,7 @@ ejecuta(function (){
   $render .= 
   '<th> $id</th>
   <td> $pregunta</td>
-  <td> <input type="checkbox"></td> '; 
+  <td> <input type="checkbox"></td>' ; 
   }
 
   return $render;
