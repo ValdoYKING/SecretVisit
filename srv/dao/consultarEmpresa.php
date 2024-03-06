0ab2451d6da01f91dd316c68bdf2bc925b0c82ae
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-03-05 23:06:54 -0600

    Cambios_Empresa

--- srv/dao/consultarEmpresa.php
+++ srv/dao/consultarEmpresa.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../../lib/php/recibeFetchAll.php";
-require_once __DIR__ . "/../modelo/Pregunta.php";
+require_once __DIR__ . "/../modelo/Empresa.php";
 require_once __DIR__ . "/AccesoBd.php";
 
 /** @return Empresa[] */
