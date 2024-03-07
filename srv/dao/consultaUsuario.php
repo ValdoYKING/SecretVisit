93293fba705b9551c3434c73521cff90ddf2273b
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-03-07 00:07:36 -0600

    vista de usuarios con tabla en administrador

---
+++ srv/dao/consultaUsuario.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . "/../modelo/Usuario.php";
+require_once __DIR__ . "/AccesoBd.php";
+
+function obtenerUsuarios()
+{
+    $con = AccesoBd::getCon();
+    $stmt = $con->query(
+        "SELECT USU_ID as id, USU_CUE as cue, USU_CORREO as correo FROM USUARIO"
+    );
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $usuarios;
+}
+?>
