c336935f3c02008ada42378de413ce50cd04f718
Replit user <>
2024-03-31 05:47:33 +0000

    add admin view

--- srv/dao/consultaUsuario.php
+++ srv/dao/consultaUsuario.php
@@ -7,7 +7,16 @@ function obtenerUsuarios()
 {
     $con = AccesoBd::getCon();
     $stmt = $con->query(
-        "SELECT USU_ID as id, USU_CUE as cue, USU_CORREO as correo FROM USUARIO
+        "SELECT 
+    USUARIO.USU_ID as id, 
+    USUARIO.USU_CUE as cue, 
+    USUARIO.USU_CORREO as correo,
+    USU_ROL.ROL_ID as rol_id  -- Seleccionar el ID del rol
+FROM 
+    USUARIO 
+INNER JOIN 
+    USU_ROL ON USUARIO.USU_ID = USU_ROL.USU_ID
+        "
     );
     $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $usuarios;
