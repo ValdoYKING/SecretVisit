41c77ed8058daf2378802978a4ec1aabd54a625e
Oscar <ovarela@e-bitware.com>
2024-03-31 19:11:03 -0600

    add create cuestionario

--- srv/srvConsultarEmpresabyId.php
+++ srv/srvConsultarEmpresabyId.php
@@ -1,28 +1,29 @@
 <?php
-
 require_once __DIR__ . "/../lib/php/ejecuta.php";
-require_once __DIR__ . "/dao/consultarEmpresa.php";
+require_once __DIR__ . "/../lib/php/leeTexto.php";
+require_once __DIR__ . "/const/CUE.php";
+require_once __DIR__ . "/modelo/Rol.php";
+require_once __DIR__ . "/modelo/Usuario.php";
+require_once __DIR__ . "/dao/usuarioBuscaCue.php";
+require_once __DIR__ . "/dao/consultarEmpresabyId.php"; 
 
-ejecuta(function () {
-  $lista = consultarEmpresa();
-  $render = "";
-  foreach ($lista as $it) {
-    $id = htmlentities($it->id);
-    $nombre = htmlentities($it->nombre);
-    $direccion = htmlentities($it->direccion);
-    $telefono = htmlentities($it->telefono);
-    $calificacion = htmlentities($it->calificacion);
-    $render .= 
-    "<div class='card' style='width: 18rem;'>
-    <div class='card-body'>
-      <h5 class='card-title'> $nombre </h5>
-      <p class='card-text'>$telefono</p>
-      <a class='btn btn-primary'> $direccion</a>
-      <div> $calificacion</div>
-      <a href='crearEncuesta.html?empId=$id&?cue=rodolfo'>Crear Cuestionario</a>
-    </div>
-  </div>
-    "; 
+// Nuevo archivo que contendrá la lógica para cambiar el rol de usuario
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (isset($data['id']) ) {
+        $id = $data['id'];
+                      $response=  consultarEmpresabyId($id,);
+                        echo json_encode($response);
+        
+    } else {
+        $response = array('status' => 'error', 'message' => 'Faltan datos en la solicitud');
+        echo json_encode($response);
     }
-    return $render;
-});
+} else {
+    $response = array('status' => 'error', 'message' => 'Se esperaba una solicitud POST');
+    echo json_encode($response);
+}
+
+
+?>
