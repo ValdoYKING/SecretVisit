27c976da51ef270e65188fc0da03789f7902eaad
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-04-02 18:01:14 -0600

    Se eliminan archivos duplicados

--- srv/const/ID_USUARIO.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-const ID_USUARIO = "idUsuario";
--- srv/srvConsultarEmpresasAnalista.php
+++ srv/srvConsultarEmpresasAnalista.php
@@ -11,13 +11,13 @@ require_once __DIR__ . "/dao/consultarEmpresaAnalista.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
-    if (isset($data['idUsuario'])) { // Cambio aquí para coincidir con el nombre del parámetro enviado
-        $idUsuario = $data['idUsuario']; // Cambio aquí para coincidir con el nombre del parámetro enviado
+    if (isset($data['id'])) { // Cambio aquí para coincidir con el nombre del parámetro enviado
+        $id = $data['id']; // Cambio aquí para coincidir con el nombre del parámetro enviado
         //$usuario->cue = $_SESSION[CUE];
         /* $usuario = new Usuario();
         $usuario->cue = $_SESSION[CUE];
         $usuario->cue = $cue; */
-        $response=  consultarEmpresaAnalista($idUsuario); // Modificación aquí para enviar el ID como parámetro
+        $response=  consultarEmpresaAnalista($id); // Modificación aquí para enviar el ID como parámetro
         echo json_encode($response);
         
     } else {
