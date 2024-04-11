a3314bf8df9de1f14a348a50cbcbfeafd42d62bd
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-04-11 01:25:02 -0600

    cambios terminos

--- srv/dao/eliminarEncuestaId.php
+++ srv/dao/eliminarEncuestaId.php
@@ -2,17 +2,42 @@
     require_once __DIR__ . "/AccesoBD.php";
 
     function EliminaEncuestaId(int $id) {
-        
-
         $con = AccesoBd::getCon();
 
         $stmt = $con->prepare(
             "DELETE 
             FROM ENCUESTA 
-            WHERE ENC_ID = :encuestaId"
+            WHERE EMP_ID = :encuestaId"
         );
 
         $stmt->execute([
             'encuestaId' => $id
+        ]);
+    }
+
+    function EliminaEmpresaId(int $id) {
+        
+        $con = AccesoBd::getCon();
+
+        $stmt = $con->prepare(
+            'DELETE FROM EMPRESA 
+            WHERE EMP_ID = :id'
+        );
+
+        $stmt->execute([
+            'id' => $id
+        ]);
+    }
+
+    function EliminaEncPreId(int $id) {
+        $con = AccesoBd::getCon();
+
+        $stmt = $con->prepare(
+            'DELETE FROM ENC_PRE 
+            WHERE ENC_ID IN (SELECT ENC_ID FROM ENCUESTA WHERE EMP_ID = :id)'
+        );
+
+        $stmt->execute([
+            'id' => $id
         ]);
     }
